90cc8c5c6ce23baf9378917bb90bdebc20969b9a
Sahil Rao <88286056+Sahilrao09@users.noreply.github.com>
2023-08-03 14:16:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function setRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function App() {
+  const [quote, setQuote] = useState(null);
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function newQuote() {
+    setQuote(setRandomQuote(quotes));
+  }
+
+  return (
+    <div>
+      <h1>Quote generator</h1>
+      <section>
+        <button onClick={newQuote}>Click me</button>
+        <h3>
+          <span>"</span>
+          {quote?.text}
+          <span>"</span>
+          <br />
+          <i>-&gt;{quote?.author}</i>
+        </h3>
+      </section>
+    </div>
+  );
+
+  // return (
+  //   <div>
+  //     <h1>quote gen</h1>
+  //   </div>
+  // );
+}
+
+export default App;
